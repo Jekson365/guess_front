f001b61c7e6daca90b6731f6da9af9ab52d1099d
Jeko <jeko.erg@gamil.com>
2024-12-23 16:56:55 +0400

    cc

--- src/axiosInstance.tsx
+++ src/axiosInstance.tsx
@@ -4,8 +4,7 @@ export const axiosInstance = axios.create({
     baseURL: "http://gapi.testpuzzle.ge:8080/",
     headers: {
         'Content-Type': 'application/json',
-    },
-    withCredentials: true
+    }
 })
 
 export const baseUrl = 'http://gapi.testpuzzle.ge:8080/'
